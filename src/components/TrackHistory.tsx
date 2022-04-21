e5e740e4213dbcc083f1d1791f8b225d5e696d45
Izarothy <izarothmail@gmail.com>
2022-04-21 13:46:35 +0200

    Fix history on larger screens and center button

--- src/components/TrackHistory.tsx
+++ src/components/TrackHistory.tsx
@@ -22,7 +22,7 @@ function TrackHistory({ mobileHistoryOpen, setMobileHistoryOpen }: TrackHistoryP
   return (
     <div
       className={`${
-        mobileHistoryOpen ? 'fixed' : 'hidden lg:fixed'
+        mobileHistoryOpen ? 'fixed' : 'hidden fixed lg:block'
       } h-full lg:w-1/5 lg:right-0 bg-slate-800 text-gray-100 flex flex-col justify-between px-4 lg:px-8 items-center py-16`}
     >
       <button className="absolute top-4 right-4 text-white" type="button" onClick={() => setMobileHistoryOpen(false)}>
@@ -33,16 +33,18 @@ function TrackHistory({ mobileHistoryOpen, setMobileHistoryOpen }: TrackHistoryP
         {/* eslint-disable-next-line operator-linebreak */}
         {tracks.length > 0 && tracks.map((track) => <Track sourceName={track[0]} destName={track[1]} key={track[2]} />)}
       </section>
-      <button
-        className="bg-red-500 px-4 py-2 rounded-md mx-auto"
-        type="button"
-        onClick={() => {
-          localStorage.removeItem('paths');
-          setTracks([]);
-        }}
-      >
-        Clear your path history
-      </button>
+      <span className="flex justify-center py-8">
+        <button
+          className="bg-red-500 px-4 py-2 rounded-md bottom-4"
+          type="button"
+          onClick={() => {
+            localStorage.removeItem('paths');
+            setTracks([]);
+          }}
+        >
+          Clear your path history
+        </button>
+      </span>
     </div>
   );
 }
